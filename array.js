c725782698960e65cab35561e19dcd8935a6b795
Ale00073 <38092010+Ale00073@users.noreply.github.com>
2020-03-19 21:37:36 -0300

    Add files via upload

---
+++ array.js
@@ -0,0 +1,7 @@
+let fecha = new Date(2020,11,1);
+let setYear = fecha.getFullYear();
+let setMonth =  fecha.getMonth();
+let setDate = fecha.getDate();
+console.log(setYear);
+console.log(setMonth);
+console.log(setDate);
